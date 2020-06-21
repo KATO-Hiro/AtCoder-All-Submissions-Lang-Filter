194f97308e9dffffe9620ad378929f40a922c447
Kato Hiroki <k.hiro1818@gmail.com>
2020-06-21 09:14:32 +0900

    Initial commit

---
+++ atcoder_all_submissions_lang_filter.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         AtCoder All Submissions Lang Filter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  過去問に対応するコンパイラを選択・フィルタリングすることができます。
+// @match        https://atcoder.jp/contests/*/submissions*
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @author       hiro_hiro
+// @license      CC0
+// @grant        none
+// ==/UserScript==
+
+window.onload = function(){
+    console.log("UserScript test");
+    alert("UserScriptが有効です。")
+}
