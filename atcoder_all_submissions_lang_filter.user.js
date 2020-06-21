de5c9424d65c675242e6123668d010aa0f20382d
Kato Hiroki <k.hiro1818@gmail.com>
2020-06-21 13:50:48 +0900

    Enable to show modal on click button (#15)

--- atcoder_all_submissions_lang_filter.user.js
+++ atcoder_all_submissions_lang_filter.user.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  過去問に対応するコンパイラを選択・フィルタリングすることができます。
 // @match        https://atcoder.jp/contests/*/submissions*
-// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/js/select2.min.js
 // @author       hiro_hiro
 // @license      CC0
 // @grant        none
@@ -17,9 +18,36 @@ $(function() {
   'use strict';
 
   const button = addButton();
+  const modal = addModal();
 })();
 
 function addButton() {
-  const buttonHtml = `<p><button type="button" class="btn btn-default btn-sm btn-auto-height" data-toggle="modal" data-target="#SubmissionsLangFilterModal">言語の選択</button></p>`;
+  const buttonHtml = `<p><button type="button" class="btn btn-default btn-sm btn-auto-height" data-toggle="modal" data-target="#submissionsLangFilterModal">言語の選択</button></p>`;
   $('form').append(buttonHtml);
+}
+
+function addModal() {
+  const modalHtml = `
+    <div class="modal fade" id="submissionsLangFilterModal" tabindex="-1" role="dialog" aria-labelledby="submissionsLangFilterModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <h4 class="modal-title" id="submissionsLangFilterModalLabel">言語の選択</h4>
+          </div>
+          <div class="modal-body" id="submissionsLangFilterModalBody">
+              <select id="submissionsLangFilterMultipleSelect" multiple="multiple"></select>
+          </div>
+          <div class="modal-footer">
+              <button type="button" id="submissionsLangFilterSelectAll" class="btn btn-info pull-left">Select All</button>
+              <button type="button" id="submissionsLangFilterClear" class="btn btn-danger pull-left">Clear</button>
+              <span id="submissionsLangFilterSaveLabel" style="margin-right:20px;"></span>
+              <button type="button" id="submissionsLangFilterSave" class="btn btn-success">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  $('body').prepend(modalHtml);
 }
